5b9b1f2f23e0fc6ef2a854f8170945bd04110330
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2022-06-12 22:30:47 +0200

    complete build scripts

--- archpkg/create-pkg.sh
+++ archpkg/create-pkg.sh
@@ -8,8 +8,9 @@ echo $OTP_SOURCE
 echo $OTP_SHA256
 [ -f ${OTP_TMP} ] && rm -f ${OTP_TMP}
 export OTP_SOURCE OTP_SHA256 GIT_TAG
-envsubst '$OTP_SOURCE,$OTP_SHA256' < /home/testuser/archpkg/PKGBUILD.tmpl > /home/testuser/build/PKGBUILD
+envsubst '$OTP_SOURCE,$OTP_SHA256,$GIT_TAG' < /home/testuser/archpkg/PKGBUILD.tmpl > /home/testuser/build/PKGBUILD
 pushd /home/testuser/build
 sudo pacman -Sy --noconfirm --noprogressbar
 makepkg -s --noconfirm --noprogressbar
+makepkg --printsrcinfo > .SRCINFO
 popd
