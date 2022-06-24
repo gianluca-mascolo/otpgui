1a255a9bb1415b99c927f7bb5786717df2b4c4d3
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2022-06-24 17:59:47 +0200

    build scripts for debian updated

--- build/deb/scripts/create-pkg.sh
+++ build/deb/scripts/create-pkg.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-pushd ~/build
-tar xzf ~/dist/otpgui-0.2.1.tar.gz
-pushd otpgui-0.2.1
-./icons/128x128/otpgui.png usr/share/icons/hicolor/32x32/apps/otpgui.png
+set -eu
+set -o pipefail
+GIT_TAG="$1"
+[ "$GIT_TAG" = "dev" ] && GIT_TAG="0.0.0"
+export GIT_TAG
+
+pushd ~/artifacts
+
+tar xzf ~/gitsrc/build/python/artifacts/otpgui-${GIT_TAG}.tar.gz
+pushd otpgui-${GIT_TAG}
 cp -r ~/gitsrc/icons .
 mkdir -p desktop
 cp ~/gitsrc/otpgui.desktop desktop/otpgui.desktop
-tar -C ~/build -czf ~/build/otpgui_0.2.1.orig.tar.gz otpgui-0.2.1
-cp -r ~/debian .
+tar -C ~/artifacts -czf ~/artifacts/otpgui_${GIT_TAG}.orig.tar.gz otpgui-${GIT_TAG}
+cp -r ~/templates/debian .
 debuild -us -uc
 popd
 popd
