c4c03c17aaeffeb327b3906b2c6c3f06780d056a
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2022-06-24 17:31:56 +0200

    generate otpversion during build

--- build/python/scripts/build.sh
+++ build/python/scripts/build.sh
@@ -9,6 +9,7 @@ if ( [ "$GIT_TAG" = "dev" ] ); then {
     poetry version ${GIT_TAG}
 }
 fi
+echo "program_version = '$(poetry version -s)'" > otpversion.py
 
 poetry build
 mv dist/otpgui* build/python/artifacts/
