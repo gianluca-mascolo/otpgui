21b570709fa66619892c39553b78cc9f4454f01b
Gianluca Mascolo <gianluca.mascolo@gmail.com>
2022-06-25 17:03:10 +0200

    changelog generator add dev log

--- tools/debian-changelog-generator.py
+++ tools/debian-changelog-generator.py
@@ -2,14 +2,19 @@
 from datetime import datetime
 import subprocess
 import re
+import argparse
 
 def GetTagDate(GitTag):
-    GitLogDate=subprocess.run(["git","show","--format=%aI","-s",f"{GitTag}^{{commit}}"],capture_output=True,universal_newlines=True,check=True)
-    LogDate=GitLogDate.stdout.strip()
-    LogDateIso=datetime.fromisoformat(LogDate)
     DebianFormat=r"%a, %d %b %Y %H:%M:%S %z"
-    #print(f"D: {LogDateIso.strftime(debfmt) }")
-    return LogDateIso.strftime(DebianFormat)
+    if GitTag != "0.0.0":
+        GitLogDate=subprocess.run(["git","show","--format=%aI","-s",f"{GitTag}^{{commit}}"],capture_output=True,universal_newlines=True,check=True)
+        LogDate=GitLogDate.stdout.strip()
+        LogDateIso=datetime.fromisoformat(LogDate)
+        #print(f"D: {LogDateIso.strftime(debfmt) }")
+        return LogDateIso.strftime(DebianFormat)
+    else:
+        LogDate=datetime.now()
+        return LogDate.strftime(DebianFormat)
 
 ChangeStart=re.compile(r"^<!-- changelog start -->$")
 ChangeEnd=re.compile(r"^<!-- changelog end -->$")
@@ -18,23 +23,35 @@ ChangeLine=re.compile(r"^- (.*)<!-- change line -->$")
 ProcessLine=False
 GitTag=None
 ## <!-- release tag -->
+DevChangeLog=f"""otpgui (0.0.0-1) UNRELEASED; urgency=medium
 
-with open("CHANGELOG.md") as cl:
-    lines = cl.readlines()
-    for line in lines:
-        if ChangeStart.match(line):
-            ProcessLine=True
-        if ChangeEnd.match(line):
-            ProcessLine=False
-            print(f"\n -- Gianluca mascolo <gianluca@gurutech.it>  {GetTagDate(GitTag)}")
-        if ProcessLine:
-            if ChangeTag.match(line):
-                if GitTag:
-                    print(f"\n -- Gianluca mascolo <gianluca@gurutech.it>  {GetTagDate(GitTag)}\n")
-                GitTagSearch = ChangeTag.search(line)
-                GitTag=GitTagSearch.group(1)
-                print(f"otpgui ({GitTag}-1) unstable; urgency=medium\n")
-            if ChangeLine.match(line):
-                LogLineSearch = ChangeLine.search(line)
-                LogLine=LogLineSearch.group(1)
-                print(f"  * {LogLine}")
+  * Development version
+
+ -- Gianluca mascolo <gianluca@gurutech.it>  {GetTagDate('0.0.0')}"""
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d","--dev", help="Development Changelog",action="store_true")
+args = parser.parse_args()
+
+if args.dev:
+    print(f"{DevChangeLog}")
+else:
+    with open("CHANGELOG.md") as cl:
+        lines = cl.readlines()
+        for line in lines:
+            if ChangeStart.match(line):
+                ProcessLine=True
+            if ChangeEnd.match(line):
+                ProcessLine=False
+                print(f"\n -- Gianluca mascolo <gianluca@gurutech.it>  {GetTagDate(GitTag)}")
+            if ProcessLine:
+                if ChangeTag.match(line):
+                    if GitTag:
+                        print(f"\n -- Gianluca mascolo <gianluca@gurutech.it>  {GetTagDate(GitTag)}\n")
+                    GitTagSearch = ChangeTag.search(line)
+                    GitTag=GitTagSearch.group(1)
+                    print(f"otpgui ({GitTag}-1) unstable; urgency=medium\n")
+                if ChangeLine.match(line):
+                    LogLineSearch = ChangeLine.search(line)
+                    LogLine=LogLineSearch.group(1)
+                    print(f"  * {LogLine}")
